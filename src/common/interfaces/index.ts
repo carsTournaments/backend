b59bafad3fa8d723efc793fa6684f314d675299b
Jose M. Gomez <jgomepav@emeal.nttdata.com>
2022-10-07 23:53:05 +0200

    feat: Se añade nueva ruta a analytics

--- src/common/interfaces/index.ts
+++ src/common/interfaces/index.ts
@@ -1,6 +1,6 @@
 export { ConfigI } from './config.interface';
 export { ControllerI } from './controller.interface';
-export { GetAllI } from './get-all.interface';
+export { GetAllI, NameValueI } from './generic.interface';
 export { PaginatorI } from './paginator.interface';
 export { RequestExtendedI } from './request-extended.interface';
 export { CacheDataI, CacheI } from '../../modules/cache/cache.interface';
